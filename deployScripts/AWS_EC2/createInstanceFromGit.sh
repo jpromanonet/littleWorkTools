57ff0cbe6169de2bac6110c334a17a4aded6575e
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2020-08-18 08:32:42 -0500

    Update createInstanceFromGit.sh

--- deployScripts/AWS_EC2/createInstanceFromGit.sh
+++ deployScripts/AWS_EC2/createInstanceFromGit.sh
@@ -3,8 +3,7 @@
 sudo yum -y install httpd
 sudo systemctl start httpd
 sudo yum -y install git
-sudo git clone https://amazon:Test-12345@gitlab.8amarketing.com/zendesk/showcase
-sudo mkdir -p /var/www/html/livestreaming-20-08-20/
-sudo cp -R ./showcase/* /var/www/html/livestreaming-20-08-20/
+sudo git clone https://user:password@repo
+sudo cp -R ./<YOUR GIT DIR>/* /var/www/html/
 sudo chown -R ec2-user:ec2-user /var/www/html/
-sudo echo $HOSTNAME > /var/www/html/index.html
+sudo echo $HOSTNAME > /var/www/html/success.html
