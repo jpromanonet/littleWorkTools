382997d248991de3f57d9baea2ce7b2363addcfc
Juan P. Romano <programming@jpromano.net>
2020-09-12 21:41:20 -0300

    Update cloneAllMyRepos.sh

--- cloneAllMyRepos.sh
+++ cloneAllMyRepos.sh
@@ -1,8 +1,17 @@
-
 #!/bin/bash
+# Clone all github.com repos for my user
 
-# Clone all github.com repositories for your user
+echo Insert your github username or organization name:
+read userName
 
-for repo in `curl -s https://api.github.com/users/<your_username>/repos?per_page=1000 |grep git_url |awk '{print $2}'| sed 's/"\(.*\)",/\1/'`;do
+echo
+echo About to start cloning all your repos to this dir...
+echo
+
+for repo in `curl -s https://api.github.com/users/$userName/repos?per_page=1000 |grep git_url |awk '{print $2}'| sed 's/"\(.*\)",/\1/'`;do
 git clone $repo;
 done;
+
+echo
+echo All your repos are cloned, task finished!
+echo
